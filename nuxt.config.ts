1282db3321bb478a1e4c6f22f6abad622a34b558
Wade Kelsch <prowade@hotmail.com>
2025-11-23 16:11:30 -0700

    Remove unused Nuxt components and clean up configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
+  css: ['~/assets/css/main.css'],
 })
